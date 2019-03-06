e2b42fe311a26a213d00a25064767cf16619ceb0
daniel <daniel.soler@e-campus.uab.cat>
2019-03-06 23:52:51 +0100

    Include correlation, bar plots and rmsdbyres

--- cpptraj/analisis.py
+++ cpptraj/analisis.py
@@ -13,54 +13,77 @@ class CpptajBuilder(object):
         self.traj =  pt.iterload(traj, top=self.topology, autoimage=True) 
 
     def strip(self, traj, ions=True, water=True, membrane=True, others="", output_path="analisis"):
-	# Check output path
-	if not os.path.exists(output_path):
-		os.mkdir(output_path)
-	traj2 = pt.autoimage(traj[:])
-        # Strip traj
-        if ions:
-            traj_nowat = pt.strip(traj2, ":Na+, Cl-")
-        if water:
-            traj_nowat = pt.strip(traj_nowat, ":WAT")
-        if membrane:
-            traj_nowat = pt.strip(traj_nowat, ":PA, PC, OL")
-        if others:
-            traj_nowat = pt.strip(traj_nowat, others)
-        # Save output
-	n_frames = self.traj.n_frames
-	output_strip_top = os.path.join(output_path, 'traj_strip_autoimaged.top')
-	output_strip_converged_traj = os.path.join(output_path, 'traj_strip_converged.nc')
-	# Write strip traj
-        pt.save(output_strip_top, traj_nowat.top , overwrite=True)
-        pt.write_traj(output_strip_converged_traj, traj_nowat, overwrite=True, frame_indices=range(n_frames/2, n_frames-1))
-	# Load new traj
-        self.traj_converged = pt.load(output_strip_converged_traj, top=output_strip_top)
- 	print("Preprocessed done succesfully")
+	    # Check output path
+	    if not os.path.exists(output_path):
+	    	os.mkdir(output_path)
+	    traj2 = pt.autoimage(traj[:])
+            # Strip traj
+            if ions:
+                traj_nowat = pt.strip(traj2, ":Na+, Cl-")
+            if water:
+                traj_nowat = pt.strip(traj_nowat, ":WAT")
+            if membrane:
+                traj_nowat = pt.strip(traj_nowat, ":PA, PC, OL")
+            if others:
+                traj_nowat = pt.strip(traj_nowat, others)
+            # Save output
+	    n_frames = self.traj.n_frames
+	    output_strip_top = os.path.join(output_path, 'traj_strip_autoimaged.top')
+	    output_strip_converged_traj = os.path.join(output_path, 'traj_strip_converged.nc')
+	    # Write strip traj
+            pt.save(output_strip_top, traj_nowat.top , overwrite=True)
+            pt.write_traj(output_strip_converged_traj, traj_nowat, overwrite=True, frame_indices=range(n_frames/2, n_frames-1))
+	    # Load new traj
+            self.traj_converged = pt.load(output_strip_converged_traj, top=output_strip_top)
+ 	    print("Preprocessed done succesfully")
 
     def RMSD(self, traj, mask='', ref=0):
         self.rmsd_data = pt.rmsd(traj, mask=mask, ref=ref)
         print("RMSD done succesfully")
 
-    def plot(self, values, output="rmsd.png", output_path="analisis"):
+    def RMSD_byresidue(self, traj, mask='', ref=0):
+        self.rmsd_byres = np.array(pt.rmsd_perres(traj, mask=mask, ref=ref))
+        print("RMSD done by res succesfully")
+
+    def plot_line(self, values, output="rmsd.png", output_path="analisis"):
         # Check initial data
         assert isinstance(values, np.ndarray), "data rmsd must be a numpy array"
-	# Check output path
-	if not os.path.exists(output_path):
-		os.mkdir(output_path)
-	# Plot
+	    # Check output path
+        if not os.path.exists(output_path):
+	    	os.mkdir(output_path)
+            # Plot
         frames = range(len(values))
         plt.plot(frames, values)
         plt.savefig(os.path.join(output_path, output))
-        print("Plot done succesfully")
+        print("Line plot done succesfully")
+
+    def plot_bars(self, values, output="rmsd.png", output_path="analisis"):
+        # Check initial data
+        assert isinstance(values, np.ndarray), "data rmsd must be a numpy array"
+	    # Check output path
+        if not os.path.exists(output_path):
+	    	os.mkdir(output_path)
+        # Plot
+        frames = range(len(values))
+        plt.bar(frames, values)
+        plt.savefig(os.path.join(output_path, output))
+        print("Bar plot done succesfully")
+
+    def correlation_matrix(self, traj, mask="@CA"):
+        self.correlation = np.array(pt.matrix.covar(traj, mask))
+        return self.correlation
 
 
-    def cluster(self, traj, mask='*', ref=0, options='sieve 200 summary {}/clusters.out repout analisis/system repfmt pdb',
-		output_path="analisis"):
+    def cluster(self, traj, mask='*', ref=0, options='sieve 10 normframe summary {0}/clusters.out repout {0}/cluster repfmt pdb',
+        output_path="analisis"):
         # Check output path
-	if not os.path.exists(output_path):
-		os.mkdir(output_path)
-	# Cluster
-        self.clusters = pt.cluster.dbscan(traj,  n_clusters=10, mask=mask, options=options.format(output_path), dtype='ndarray') 
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+        # Cluster
+        #output_cluster = os.path.join(output_path, "clusters.pdb")
+        self.clusters = pt.cluster.hieragglo(traj, mask=mask, options=options.format(output_path), dtype='ndarray')
+        #frames = [int(cluster) for cluster in self.clusters._cpp_out[1].split()[-10:]]
+        #pt.write_traj(output_cluster, traj, overwrite=True, frame_indices=frames)
         print("Clustering done succesfully")
 
 def parse_args():
@@ -75,7 +98,7 @@ def analise(traj, top):
     trajectory = CpptajBuilder(traj, top)
     trajectory.strip(trajectory.traj)
     trajectory.RMSD(trajectory.traj, mask="@CA")
-    trajectory.plot(trajectory.rmsd_data)
+    trajectory.plot_line(trajectory.rmsd_data)
     trajectory.cluster(trajectory.traj_converged)
     print("Trajectory {} sucessfuly analised".format(traj))
 
