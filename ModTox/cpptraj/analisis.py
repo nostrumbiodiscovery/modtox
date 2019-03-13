09dd8e0a8f774747166f36d455a5f55aea091cbf
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2019-03-13 18:50:52 +0100

    Implement last snapshot

--- ModTox/cpptraj/analisis.py
+++ ModTox/cpptraj/analisis.py
@@ -14,11 +14,14 @@ class CpptajBuilder(object):
         self.topology = topology if topology else traj[0]
         self.traj =  pt.iterload(traj, top=self.topology, autoimage=True) 
 
-    def strip(self, traj, ions=True, water=True, membrane=True, others="", output_path="analisis"):
+    def strip(self, traj, ions=True, water=True, membrane=True, others="", output_path="analisis", autoimage=True):
         # Check output path
         if not os.path.exists(output_path):
             os.mkdir(output_path)
-        traj2 = pt.autoimage(traj[:])
+        if autoimage:
+        	traj2 = pt.autoimage(traj[:])
+	else:
+		traj2 = traj[:]
         # Strip traj
         if ions:
             traj_nowat = pt.strip(traj2, ":Na+, Cl-")
@@ -123,7 +126,8 @@ class CpptajBuilder(object):
             os.mkdir(output_path)
         # Cluster
         #output_cluster = os.path.join(output_path, "clusters.pdb")
-        self.clusters = pt.cluster.hieragglo(traj, mask=mask, options=options.format(output_path), dtype='ndarray')
+        #self.clusters = pt.cluster.hieragglo(traj, mask=mask, options=options.format(output_path), dtype='ndarray')
+        self.clusters = pt.cluster.kmeans(traj, mask=mask, options=options.format(output_path), dtype='ndarray')
         #frames = [int(cluster) for cluster in self.clusters._cpp_out[1].split()[-10:]]
         #pt.write_traj(output_cluster, traj, overwrite=True, frame_indices=frames)
         print("Clustering done succesfully")
@@ -133,18 +137,25 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Specify trajectory and topology to be analised')
     parser.add_argument('traj', nargs="+", help='Trajectory to analise')
     parser.add_argument('--top', type=str, help='Topology of your trajectory')
+    parser.add_argument('--RMSD', action="store_true", help='Calculate RMSD plot')
+    parser.add_argument('--cluster', action="store_true", help='Perform clustering')
+    parser.add_argument('--last', action="store_true", help='Extract last snapshot')
     args = parser.parse_args()
-    return args.traj, args.top
+    return args.traj, args.top, args.RMSD, args.cluster, args.last
 
-def analise(traj, top):
+def analise(traj, top, RMSD, cluster, last):
     trajectory = CpptajBuilder(traj, top)
-    trajectory.strip(trajectory.traj)
-    trajectory.RMSD(trajectory.traj, mask="@CA")
-    trajectory.plot_line(trajectory.rmsd_data)
-    trajectory.cluster(trajectory.traj_converged)
+    if RMSD:
+    	trajectory.strip(trajectory.traj, autoimage=False)
+    	trajectory.RMSD(trajectory.traj, mask="@CA")
+    	trajectory.plot_line(trajectory.rmsd_data)
+    if cluster:
+    	trajectory.cluster(trajectory.traj_converged)
+    if last:
+    	trajectory.save_traj(trajectory.traj, frame_indices=[trajectory.traj.n_frames-1], output_path=".", output="last_snap.pdb")
     print("Trajectory {} sucessfuly analised".format(traj))
 
 
 if __name__ == "__main__":
-    traj, top = parse_args()
-    analise(traj, top)
+    traj, top, RMSD, cluster, last = parse_args()
+    analise(traj, top, RMSD, cluster, last)
