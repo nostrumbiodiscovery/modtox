f1ac62c52788dee5213250b4b609f215c8563772
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2019-04-02 12:51:29 +0200

    Add physics based or glide based model option & and umber of k fold when crossvalidating

--- ModTox/main.py
+++ ModTox/main.py
@@ -18,29 +18,30 @@ def parse_args():
     parser.add_argument('--dock',  action="store_true", help='Topology of your trajectory')
     parser.add_argument('--analysis', action="store_true", help='Calculate RMSD plot')
     args = parser.parse_args()
-    return args.traj, args.resname, args.active, args.inactive, args.top, args.glide_files, args.model, args.best, args.csv, args.RMSD, args.cluster, args.last, args.clust_type, args.rmsd_type, args.receptor, args.ligands_to_dock, args.grid, args.precision, args.maxkeep, args.maxref, args.dock, args.analysis, args.test, args.save, args.load, args.external_data
+    return args.traj, args.resname, args.active, args.inactive, args.top, args.glide_files, args.best, args.csv, args.RMSD, args.cluster, args.last, args.clust_type, args.rmsd_type, args.receptor, args.ligands_to_dock, args.grid, args.precision, args.maxkeep, args.maxref, args.dock, args.analysis, args.test, args.save, args.load, args.external_data, args.pb, args.cv
 
-def main(traj, resname, active, inactive, top=None, glide_files="*dock*.mae", model=False, best=True, csv=False, RMSD=True, cluster=True, last=True, clust_type="BS", rmsd_type="BS", receptor="*pv*.maegz", grid=None, precision="SP", maxkeep=500, maxref=400, dock=False, analysis=True, test=None, save=None, load=None, external_data=None):
+def main(traj, resname, active, inactive, top=None, glide_files="*dock*.maegz", best=False, csv=False, RMSD=True, cluster=True, last=True, clust_type="BS", rmsd_type="BS", receptor="*pv*.maegz", grid=None, precision="SP", maxkeep=500, maxref=400, dock=False, analysis=True, test=None, save=None, load=None, external_data=None, pb=False, cv=2):
     if dock:
         # Analyze trajectory&extract clusters
         an.analise(traj, resname, top, RMSD, cluster, last, clust_type, rmsd_type)
         # Cross dock all ligand to the extracted clusters
         docking_obj = dk.Glide_Docker(glob.glob("analisis/*clust*.pdb"), [active, inactive])
         docking_obj.dock(precision=precision, maxkeep=maxkeep, maxref=maxref)
-        print("Docking in process... Run the same command with --analysis flag to build model")
+        print("Docking in process... Once is finished run the same command exchanging --dock by --analysis flag to build model")
     elif analysis:
         # Analyze dockig files and build model features
-        gl.analyze(glide_files, model=model, best=best, csv=csv, active=active, inactive=inactive)
+        inp_files = glob.glob(glide_files)
+        gl.analyze(inp_files, best=best, csv=csv, active=active, inactive=inactive)
         # Build Model
         clf = svm.SVC(C=1, gamma=1, kernel="linear")
         model = md.GenericModel(active, inactive, clf, csv=external_data, test=test)
-        model.fit_transform(cv=2)
+        model.fit_transform(cv=cv, pb=pb)
         print("Model sucesfully build. Confusion_matrix.png outputted")
 
 
 
 if __name__ == "__main__":
-    trajs, resname, active, inactive, top, glide_files, model, best, csv, RMSD, cluster, last, clust_type, rmsd_type, \
-       receptor, ligands_to_dock, grid, precision, maxkeep, maxref, dock, analysis, test, save, load, external_data = parse_args()
-    main(trajs, resname, active, inactive, top, glide_files, model, best, csv, RMSD, cluster, last, clust_type, rmsd_type, 
-        receptor, grid, precision, maxkeep, maxref, dock, analysis, test, save, load, external_data)
+    trajs, resname, active, inactive, top, glide_files, best, csv, RMSD, cluster, last, clust_type, rmsd_type, \
+       receptor, ligands_to_dock, grid, precision, maxkeep, maxref, dock, analysis, test, save, load, external_data, pb, cv = parse_args()
+    main(trajs, resname, active, inactive, top, glide_files, best, csv, RMSD, cluster, last, clust_type, rmsd_type, 
+        receptor, grid, precision, maxkeep, maxref, dock, analysis, test, save, load, external_data, pb, cv)
