bf518169c29f33bee57e1340f1f0a0b1bc9842fd
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2019-05-31 18:00:33 +0200

    Activate all tests

--- examples/modtox_commandline.sh
+++ examples/modtox_commandline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Analyse MD
-#python -m modtox.cpptraj.analisis ../tests/data/general/traj.pdb 198 --top ../tests/data/general/init.top --RMSD --clust_type BS --last
+python -m modtox.cpptraj.analisis ../tests/data/general/traj.pdb 198 --top ../tests/data/general/init.top --RMSD --clust_type BS --last
 
 # Ensemble docking from dude
-#python -m modtox.main ../tests/data/general/traj.pdb 198 --dude ../tests/data/dude --top ../tests/data/general/init.top --grid_mol 2 --dock --clust_sieve 1 
+python -m modtox.main ../tests/data/general/traj.pdb 198 --dude ../tests/data/dude --top ../tests/data/general/init.top --grid_mol 2 --dock --clust_sieve 1 
 
 #Ensemble docking active inactive 
 python -m modtox.main ../tests/data/general/traj.pdb 198 --active ../tests/data/active_decoys/active.sdf --inactive ../tests/data/active_decoys/decoys.sdf --top ../tests/data/general/init.top --grid_mol 3 --dock --clust_sieve 1
