f560bb0600fb6baa017389a0f5a7ad62644e059d
Nostrum <nostrum@mbp-de-nostrum.domain.com>
2019-09-18 21:04:55 +0200

    add find packages to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 
-setup(name='ModTox Platform',
-      version='1.0.0',
+setup(name='modtox',
+      packages=find_packages(),
+      version='1.0.0.3',
+      license='MIT',
       description='Asses toxic effect of drugs based on MD simualtions of HROT dataset',
       author='Daniel Soler',
-      author_email='daniel.soler@e-campus.uab.cat',
-      url='https://www.python.org/sigs/distutils-sig/',
-      install_requires=['pytraj', 'mdtraj'],
+      author_email='daniel.soler@nostrumbiodiscovery.com',
+      url='https://github.com/NostrumBioDiscovery/modtox',
+      install_requires=['pytraj', 'mdtraj', 'matplotlib', 'xgboost', 'sklearn', 'prody', 'biopython', 'pandas', 'mordred', 'nltk', 'seaborn', 'umap-learn', 'tqdm', 'requests', 'chembl_webresource_client', 'pytest'],
      )
