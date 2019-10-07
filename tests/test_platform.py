0794fa3a8286a80622630184b6a486fe262bcb78
modeorru <modesto.orozco@e-campus.uab.cat>
2019-10-07 11:43:13 +0200

    Solved minor problems in parsin

--- tests/test_platform.py
+++ tests/test_platform.py
@@ -40,7 +40,7 @@ def test_pubchem(traj, resname, top, pubchem):
 def test_model_stack(traj, resname, top, active, inactive):
      initial_dir = os.getcwd()
      os.chdir(os.path.join(DATA_PATH, "analysis"))
-     mn.main(traj, resname, active, inactive, top=top, analysis=True, glide_files=GLIDE_FILES, debug=True, cv=2, classifier="stack")
+     mn.main(traj, resname, active, inactive, top=top, assemble_model=True, glide_files=GLIDE_FILES, debug=True, cv=2, classifier="stack")
      os.chdir(initial_dir)
 
 @pytest.mark.parametrize("traj, resname, top, active, inactive", [
@@ -49,5 +49,5 @@ def test_model_stack(traj, resname, top, active, inactive):
 def test_model_normal(traj, resname, top, active, inactive):
      initial_dir = os.getcwd()
      os.chdir(os.path.join(DATA_PATH, "analysis"))
-     mn.main(traj, resname, active, inactive, top=top, analysis=True, glide_files=GLIDE_FILES, debug=True, cv=2)
+     mn.main(traj, resname, active, inactive, top=top, assemble_model=True, glide_files=GLIDE_FILES, debug=True, cv=2)
      os.chdir(initial_dir)
