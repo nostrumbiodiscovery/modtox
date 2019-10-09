501cd22ad70829300e9e8043580b35a2adbe8df2
modeorru <modesto.orozco@e-campus.uab.cat>
2019-10-09 18:07:58 +0200

    Solved minor problems

--- modtox/main.py
+++ modtox/main.py
@@ -47,10 +47,10 @@ def parse_args():
     parser.add_argument('--status', default = "train", help= "Train or test")
     args = parser.parse_args()
     
-    return args.traj, args.resname, args.active, args.inactive, args.top, args.glide_files, args.best, args.csv, args.RMSD, args.cluster, args.last, args.clust_type, args.rmsd_type, args.receptor, args.ligands_to_dock, args.grid, args.precision, args.maxkeep, args.maxref, args.dock, args.assemble_model, args.predict, args.test, args.save, args.load, args.external_data, args.pb, args.cv, args.features, args.features_cv, args.descriptors, args.classifier, args.filename_model, args.save_model, args.dude, args.pubchem, args.stored_files, args.csv_filename, args.substrate, args.grid_mol, args.clust_sieve, args.debug, args.output_dir, args.status, args.mol_to_read
+    return args.traj, args.resname, args.active, args.inactive, args.top, args.glide_files, args.best, args.csv, args.RMSD, args.cluster, args.last, args.clust_type, args.rmsd_type, args.receptor, args.ligands_to_dock, args.grid, args.precision, args.maxkeep, args.maxref, args.dock, args.assemble_model, args.predict, args.test, args.save, args.load, args.external_data, args.pb, args.cv, args.features, args.features_cv, args.descriptors, args.classifier, args.filename_model, args.dude, args.pubchem, args.stored_files, args.csv_filename, args.substrate, args.grid_mol, args.clust_sieve, args.debug, args.output_dir, args.status, args.mol_to_read
 
 
-def main(traj, resname, active=None, inactive=None, top=None, glide_files="*dock*.maegz", best=False, csv=False, RMSD=True, cluster=True, last=True, clust_type="BS", rmsd_type="BS", receptor="*pv*.maegz", grid=None, precision="SP", maxkeep=500, maxref=400, dock=False, assemble_model=True, predict = False, test=None, save=None, load=None, external_data=None, pb=False, cv=2, features=5, features_cv=1, descriptors=[], classifier="svm", save_model = None, filename_model = None, dude=None, pubchem = None, stored_files = False, csv_filename=None, substrate=None, grid_mol=2, sieve=10, debug=False, output_dir = "modtox_results", status = None, mol_to_read=None):
+def main(traj, resname, active=None, inactive=None, top=None, glide_files="*dock*.maegz", best=False, csv=False, RMSD=True, cluster=True, last=True, clust_type="BS", rmsd_type="BS", receptor="*pv*.maegz", grid=None, precision="SP", maxkeep=500, maxref=400, dock=False, assemble_model=True, predict = False, test=None, save=None, load=None, external_data=None, pb=False, cv=2, features=5, features_cv=1, descriptors=[], classifier="svm", filename_model = None, dude=None, pubchem = None, stored_files = False, csv_filename=None, substrate=None, grid_mol=2, sieve=10, debug=False, output_dir = "modtox_results", status = None, mol_to_read=None):
     
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -64,7 +64,7 @@ def main(traj, resname, active=None, inactive=None, top=None, glide_files="*dock
             if dude:
                 active, inactive = dd.process_dude(dude, status, test=debug)
             if pubchem:
-                active, inactive = pchm.process_pubchem(pubchem, csv_filename, status, substrate, stored_files, test=debug, mol_to_read=mol_to_read)
+                active, inactive = pchm.process_pubchem(pubchem, stored_files = stored_files, csv_filename = csv_filename, status = status, substrate = substrate, mol_to_read=mol_to_read, test = debug)
             if active.split(".")[-1] == "csv":
                 active = gpcr.process_gpcrdb(active)
                 inactive = inactive
@@ -109,7 +109,7 @@ if __name__ == "__main__":
     trajs, resname, active, inactive, top, glide_files, best, csv, RMSD, cluster, last, clust_type, rmsd_type, \
     receptor, ligands_to_dock, grid, precision, maxkeep, maxref, dock, assemble_model, predict, test, \
     save, load, external_data, pb, cv, features, features_cv, descriptors, \
-    classifier, filename_model, save_model, dude, pubchem, stored_files, csv_filename, substrate, grid_mol, sieve, debug, output_dir, status, mol_to_read = parse_args()
+    classifier, filename_model, dude, pubchem, stored_files, csv_filename, substrate, grid_mol, sieve, debug, output_dir, status, mol_to_read = parse_args()
     main(trajs, resname, active, inactive, top, glide_files, best, csv, RMSD, cluster, last, clust_type, rmsd_type, 
         receptor, grid, precision, maxkeep, maxref, dock, assemble_model, predict, test, save, load, external_data, pb, cv, features, features_cv, 
-        descriptors, classifier, save_model, filename_model, dude, pubchem, stored_files, csv_filename, substrate, grid_mol, sieve, debug, output_dir, status, mol_to_read)
+        descriptors, classifier, filename_model, dude, pubchem, stored_files, csv_filename, substrate, grid_mol, sieve, debug, output_dir, status, mol_to_read)
