d4ccde4202e461ddcf0eb79d9b5d0c62f08deea9
modeorru <modesto.orozco@e-campus.uab.cat>
2019-10-09 16:22:42 +0200

    Solved problems when parsing integers as names

--- modtox/ML/external_descriptors.py
+++ modtox/ML/external_descriptors.py
@@ -23,16 +23,16 @@ class ExternalData():
         #If not present on your dataset discard
         self.mol_names = [m[0].GetProp("_Name") for m in molecules.values]
         for i, title in enumerate(df["Title"].values):
-            if title not in self.mol_names:
+            if str(title) not in self.mol_names:
                 df.drop(df.index[i- n_drop], inplace=True)
                 n_drop += 1
         headers = list(df)
         values = (None,) * len(headers)
         #If not present on your glide discard
         for i, title in enumerate(self.mol_names):
-            if title not in df["Title"].values:
-                line = pd.DataFrame.from_records([values], columns=headers, index=[i])
-                df = pd.concat([df.ix[:i-1], line, df.ix[i:]]).reset_index(drop=True)
+            if title not in df["Title"].values.astype(str):
+                null_line = pd.DataFrame.from_records([values], columns=headers, index=[i])
+                df = pd.concat([df.ix[:i-1], null_line, df.ix[i:]]).reset_index(drop=True)
         #Drop features
         df = df.replace("--",  np.nan)
         features_to_drop = [feature for field in self.exclude for feature in headers if field in feature ]
