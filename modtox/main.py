948e50af8895228362135011d2e1d0aeede0f376
modeorru <modesto.orozco@e-campus.uab.cat>
2019-10-31 17:51:59 +0100

    Old changes

--- modtox/main.py
+++ modtox/main.py
@@ -12,6 +12,7 @@ import modtox.docking.glide.glide as dk
 import modtox.docking.glide.analyse as gl
 import modtox.data.dude as dd
 import modtox.Helpers.helpers as hp
+import modtox.Helpers.command_check as cc
 import modtox.data.gpcrdb as gpcr
 import modtox.data.pubchem as pchm
 
@@ -57,13 +58,18 @@ def parse_args():
     return args.traj, args.resname, args.active, args.inactive, args.top, args.glide_files, args.best, args.csv, args.RMSD, args.cluster, args.last, args.clust_type, args.rmsd_type, args.receptor, args.ligands_to_dock, args.grid, args.precision, args.maxkeep, args.maxref, args.dock, args.assemble_model, args.predict, args.save, args.load, args.external_data, args.pb, args.cv, args.features, args.features_cv, args.descriptors, args.classifier, args.filename_model, args.dude, args.pubchem, args.substrate, args.grid_mol, args.clust_sieve, args.debug, args.output_dir,args.train, args.test, args.mol_to_read, args.tpot
 
 
-def main(traj, resname, active=None, inactive=None, top=None, glide_files="*dock_lib.maegz", best=False, csv=False, RMSD=True, cluster=True, last=True, clust_type="BS", rmsd_type="BS", receptor="*pv*.maegz", grid=None, precision="SP", maxkeep=500, maxref=400, dock=False, assemble_model=False, predict = False, save=None, load=None, external_data=None, pb=False, cv=2, features=5, features_cv=1, descriptors=[], classifier="svm", filename_model = None, dude=None, pubchem = None, substrate=None, grid_mol=2, sieve=10, debug=False, output_dir="cyp2c9", train=False, test=False, mol_to_read=None, tpot=False):
+def main(traj, resname, active=None, inactive=None, top=None, glide_files="*dock_lib.maegz", best=False, csv=False, RMSD=True, cluster=True, last=True, clust_type="BS", rmsd_type="BS", receptor="*pv*.maegz", grid=None, precision="SP", maxkeep=500, maxref=400, dock=False, assemble_model=False, predict = False, save=None, load=None, external_data=None, pb=False, cv=2, features=5, features_cv=1, descriptors=[], classifier="single", filename_model = None, dude=None, pubchem = None, substrate=None, grid_mol=2, sieve=10, debug=False, output_dir="cyp2c9", train=False, test=False, mol_to_read=None, tpot=False):
     
+    
+    assert output_dir not in os.getcwd(), "Wrong directory for running the script"
     # Create test training folder
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
+
     if train: direct = os.path.join(output_dir, "from_train")
-    if test: direct = os.path.join(output_dir, "from_test")
+    if test: 
+        direct = os.path.join(output_dir, "from_test")
+        assert os.path.exists(os.path.join(output_dir, "from_train")), "Wrong directory for running the script"
     if not os.path.exists(direct): os.mkdir(direct)
     if dock:
         # Retrieve receptor, ligands and dock them
@@ -74,7 +80,7 @@ def main(traj, resname, active=None, inactive=None, top=None, glide_files="*dock
 
             print("Extracting clusters from MD")
             if not os.path.exists(ANALISIS_FOLDER):
-                an.analise(traj, resname, top, RMSD, cluster, last, clust_type, rmsd_type, sieve, train, test)
+                an.analise(traj, resname, top, RMSD, cluster, last, clust_type, rmsd_type, sieve)
 
             print("Prepare active and inactive ligands")
             if dude:
@@ -95,14 +101,13 @@ def main(traj, resname, active=None, inactive=None, top=None, glide_files="*dock
 
     if assemble_model:
         assert train, "Assemble model only allowed with train flag" 
-        if dude or pubchem:
-            active = "active_train.sdf"
-            inactive = "inactive_train.sdf"
-    
+        active = "dataset/active_train.sdf"
+        inactive = "dataset/inactive_train.sdf"
+
         with hp.cd(direct):
             #Saving commandline arguments
             with open('commandline_args_assemble.txt', 'w') as f:
-                f.write("".join(sys.argv[1:]))
+                f.write(" ".join(sys.argv[1:]))
             print("Build input fingerprints from docking terms")
             inp_files = glob.glob(os.path.join(DOCKING_FOLDER, glide_files))
             gl.analyze(inp_files, best=best, csv=csv, active=active, inactive=inactive, debug=debug)
@@ -119,16 +124,17 @@ def main(traj, resname, active=None, inactive=None, top=None, glide_files="*dock
             print("Models sucesfully build and saved.")
      
     if predict:
-        if dude or pubchem:
-            active = "dataset/active_test.sdf"
-            inactive = "dataset/inactive_test.sdf"
+        active = "dataset/active_test.sdf"
+        inactive = "dataset/inactive_test.sdf"
 
         with hp.cd(direct):
             print(direct)
             assert os.path.isfile(os.path.join("../from_train/model", filename_model))== True, "Run the training of the module first with flag --assemble_model. More in docs"
             #Saving commandline arguments
             with open('commandline_args_predict.txt', 'w') as f:
-                f.write(' '.join(sys.argv[1:]))
+                f.write(" ".join(sys.argv[1:]))
+
+            cc.command_check() #checking if classifiers selection is appropriate
             
             print("Build input fingerprints from docking terms")
             inp_files = glob.glob(os.path.join(DOCKING_FOLDER, glide_files))
