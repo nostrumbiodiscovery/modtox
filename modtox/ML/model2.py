f6a10d6274dc39758b4a0857044a13d495c2b894
modeorru <modesto.orozco@e-campus.uab.cat>
2020-01-20 09:19:12 +0100

    Minor changes

--- modtox/ML/model2.py
+++ modtox/ML/model2.py
@@ -1,8 +1,10 @@
 import os
 import numpy as np
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import f1_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
 import modtox.ML.classifiers as cl
 import pandas as pd
 import pickle
@@ -39,15 +41,12 @@ class Imputer(object):
             self.imputer = ImputerForSample(strategy=strategy, fill_value=fill_value, missing_values=missing_values, n_clusters=n_clusters)
         
     def fit(self, X):
-
         return self.imputer.fit(X)   
 
     def transform(self,X):
-
         return self.imputer.transform(X)
 
     def fit_transform(self, X):
-
         return self.imputer.fit_transform(X)
 
 
@@ -98,16 +97,19 @@ class ImputerForSample(object):
 
 class GenericModel(object):
 
-    def __init__(self, clf, filename_model='opt_model.pkl', folder='.',  tpot=False, cv=5, generations=3, random_state=42, population_size=10, majvoting=False, debug=False):
+    def __init__(self, X, Y, clf, filename_model='opt_model.pkl', folder='.',  tpot=False, cv=5, scoring='balanced_accuracy', generations=3, random_state=42, population_size=10, X_removed=None, y_removed=None, majvoting=False, weighting=False, debug=False):
         self.X = None
         self.Y = None
         self.fitted = False
         self.folder = folder
         self.filename_model = filename_model
         self.tpot = tpot
+        self.y_removed = y_removed
+        self.X_removed = X_removed
         self.cv = cv
         self.majvoting = majvoting
-        self.clf = cl.retrieve_classifier(clf, self.tpot, cv=self.cv, generations=generations, random_state=random_state, population_size=population_size, fast=False)
+        self.weighting = weighting
+        self.clf = cl.retrieve_classifier(clf,self.tpot, cv=self.cv, scoring=scoring, generations=generations, random_state=random_state, population_size=population_size, fast=False, model=None)
         self.stack = self._is_stack_model()   
 
         self.scaler = StandardScaler()
@@ -115,7 +117,7 @@ class GenericModel(object):
         self.debug = debug
 
     def _is_stack_model(self):
-        return type(self.clf) is list and len(self.clf) > 0
+        return type(self.clf) is list and len(self.clf) > 2
 
     def _extract_pred_proba(self, X, y, f=None, models = None):
         if f != None: 
@@ -134,9 +136,21 @@ class GenericModel(object):
                 proba = np.array([[[0,1] if pre == 1 else [1,0] for pre in pred[i]]for i in range(len(models))])
         else:
             assert y.any(), "Need y"
-            pred = np.array([ cross_val_predict(c, X, y, cv=self.cv) for c in models])
-            proba = np.array([ cross_val_predict(c, X, y, cv=self.cv, method='predict_proba') for c in models])
-
+            if self.fitted:
+                proba = np.array([c.predict(X) for c in models])
+            else:
+                pred = np.array([ cross_val_predict(c, X, y, cv=self.cv) for c in models])
+            for k, pp in enumerate(pred):
+                cm = confusion_matrix(y, pp)
+                print(models[k], cm)
+            try:
+                if self.fitted:
+                    proba = np.array([c.predict_proba(X) for c in models])
+                else:
+                    proba = np.array([ cross_val_predict(c, X, y, cv=self.cv, method='predict_proba') for c in models])
+            except AttributeError:
+                proba = np.array([[[0,1] if pre == 1 else [1,0] for pre in pred[i]] for i in range(len(models))])
+                
         return pred,proba
  
     def _stack(self, X, pred, proba, stack_type='proba'):
@@ -150,7 +164,7 @@ class GenericModel(object):
         clf_result = np.vstack([y_indiv, y_final])
         return [([pred == true for pred, true  in zip(result, Y_true.tolist())]) for result in clf_result]
 
-    def _last_fit(self, X, y, f=None):
+    def _last_fit(self, X, y, y_removed, f=None):
        if self.tpot:
            self.last_clf.fit(X,y)
            prediction = self.last_clf.predict(X)
@@ -162,15 +176,22 @@ class GenericModel(object):
            pickle.dump(self.last_clf.fitted_pipeline_, f)
        else:
            prediction = cross_val_predict(self.last_clf, X, y, cv=self.cv)
-           prediction_proba = cross_val_predict(self.last_clf, X, y, cv=self.cv, method='predict_proba')
+           try:
+               prediction_proba = cross_val_predict(self.last_clf, X, y, cv=self.cv, method='predict_proba')
+           except AttributeError: 
+               prediction_proba =  np.array([[0,1] if pred == 1 else [1,0] for pred in prediction])
            last_fitted = self.last_clf.fit(X,y)
            pickle.dump(last_fitted, f)
 
-       #pickle.dump(self.scaler, f)
+       if len(y_removed)>0:
+           y_pred_removed = np.zeros((len(y_removed),), dtype=int)
+           y_proba_removed = [[1,0] for i in range(len(y_removed))]
+           prediction = np.concatenate((prediction, y_pred_removed))
+           prediction_proba = np.concatenate((prediction_proba, y_proba_removed))
 
        return prediction, prediction_proba
 
-    def _last_predict(self, X):
+    def _last_predict(self, X, y_removed):
         prediction = self.last_model.predict(X)
         try:
             proba = self.last_model.predict_proba(X)
@@ -178,31 +199,80 @@ class GenericModel(object):
             proba = np.array([[0,1] if pred == 1 else [1,0] for pred in prediction])
             pass
 
+        if len(y_removed)>0:
+            y_pred_removed = np.zeros((len(y_removed),), dtype=int)
+            y_proba_removed = [[1,0] for i in range(len(y_removed))]
+            prediction = np.concatenate((prediction, y_pred_removed))
+            proba = np.concatenate((proba, y_proba_removed))
+
         return prediction, proba
 
+    def _f1_weigthing(self, y_true=None, y_preds=None):
+      
+      if not self.fitted:
+          f1_scores = [f1_score(y_true, y_pred, average='weighted') for y_pred in y_preds]
+          self.weight = f1_scores / np.sum(f1_scores)
+      print('F1 weight', self.weight)
+      y_weight =  np.array([x*y for x,y in zip(self.weight,y_preds)])
+      y_weight = np.array([np.sum(x) for x in y_weight.T])
+      y_weight_pred = np.array([[1-pred,pred] for pred in y_weight])
+      return np.array([round(x) for x in y_weight]), y_weight_pred
+      
+    def _pipeline_fit(self, X, Y, y_removed, f):
+        models = self.clf[:-1]
+        self.indiv_fit, self.proba_fit =  self._extract_pred_proba(X, Y, f=f, models=models)
+        if self.weighting:
+            self.prediction_fit, self.prediction_proba_fit = self._f1_weigthing(Y, self.indiv_fit)       
 
-    def _pipeline_fit(self, X, Y, f):
-        self.indiv_fit, self.proba_fit =  self._extract_pred_proba(X, Y, f=f, models=self.clf[:-1])
         if self.majvoting: #majority voting
+            print('Majvoting') 
             commons = [np.bincount(x) for x in self.indiv_fit.T]
             self.prediction_fit = np.array([np.argmax(i) for i in commons])
             self.prediction_proba_fit = np.array([[max(i)/sum(i),min(i)/sum(i)] for i in commons]) #now proba is just disagreement between clfs
-        else:
+
+        if not self.weighting and not self.majvoting:
             X_stack = self._stack(X, self.indiv_fit, self.proba_fit)
             self.prediction_fit, self.prediction_proba_fit = self._last_fit(X_stack, Y, f)
+         
+        #adding trivial predictions of 0's from nondocked molecules
+        if len(y_removed)>0:
+            y_pred_removed = np.zeros((len(y_removed),), dtype=int)  
+            y_proba_removed = [[1,0] for i in range(len(y_removed))]  
+            indiv_fit_removed = [y_pred_removed for i in range(len(models))]
+            proba_fit_removed = [y_proba_removed for i in range(len(models))]
+            self.prediction_fit = np.concatenate((self.prediction_fit, y_pred_removed))
+            self.prediction_proba_fit = np.concatenate((self.prediction_proba_fit, y_proba_removed))        
+            self.indiv_fit = [np.concatenate((x,y)) for x,y in zip(self.indiv_fit, indiv_fit_removed)] 
+            Y = np.concatenate((Y, y_removed))
+
         self.clf_results = self._stack_final_results(self.indiv_fit, self.prediction_fit, Y)
 
-    def _pipeline_predict(self, X, Y):
-  
+    def _pipeline_predict(self, X, Y, y_removed):
+        models = self.loaded_models
+        if self.weighting:
+            self.indiv_pred, self.proba_predict = self._extract_pred_proba(X, Y, models=models)
+            self.prediction_test, self.predictions_proba_test = self._f1_weigthing(Y, self.indiv_pred)       
+
         if self.majvoting: #majority voting
-            self.indiv_pred, self.proba_predict = self._extract_pred_proba(X, Y, models=self.loaded_models)
+            self.indiv_pred, self.proba_predict = self._extract_pred_proba(X, Y, models=models)
             commons = [np.bincount(x) for x in self.indiv_pred.T] 
             self.prediction_test = np.array([np.argmax(i) for i in commons])
             self.predictions_proba_test = np.array([[max(i)/sum(i),min(i)/sum(i)] for i in commons]) #now proba is just disagreement between clfs
-        else:
-            self.indiv_pred, self.proba_predict = self._extract_pred_proba(X, Y, models=self.loaded_models[:-1])
+        if not self.weighting and not self.majvoting:
+            self.indiv_pred, self.proba_predict = self._extract_pred_proba(X, Y, models=models[:-1])
             X_pred_stack = self._stack(X, self.indiv_pred, self.proba_predict)
             self.prediction_test, self.predictions_proba_test = self._last_predict(X_pred_stack)
+
+        if len(y_removed)>0:
+            y_pred_removed = np.zeros((len(y_removed),), dtype=int)
+            y_proba_removed = [[1,0] for i in range(len(y_removed))]
+            indiv_pred_removed = [y_pred_removed for i in range(len(models))]
+            proba_pred_removed = [y_proba_removed for i in range(len(models))]
+            self.prediction_test = np.concatenate((self.prediction_test, y_pred_removed))
+            self.predictions_proba_test = np.concatenate((self.predictions_proba_test, y_proba_removed))
+            self.indiv_pred = [np.concatenate((x,y)) for x,y in zip(self.indiv_pred, indiv_pred_removed)] 
+            Y = np.concatenate((Y, y_removed))
+
         self.clf_results =  self._stack_final_results(self.indiv_pred, self.prediction_test, Y)
 
     def load_models(self):
@@ -220,23 +290,32 @@ class GenericModel(object):
         self.X = X
         self.Y = y
         f = open(os.path.join(self.folder, self.filename_model), 'wb')
-        
         #imputing and scaling
+     
         self.X_trans = self.scaler.fit_transform(self.imputer.fit_transform(self.X))        
+        if len(self.X_removed) > 0:
+            self.X_trans_removed = np.zeros(shape=self.X_removed.shape)
+
+        if not self.tpot:
+            print('Optimizing classifier')
+            self.clf = cl.optimize_clf(self.X_trans,self.Y, self.stack, self.clf)
 
         if self.stack:
             self.last_clf = self.clf[-1]
-            self._pipeline_fit(self.X_trans, self.Y, f)
+            self._pipeline_fit(self.X_trans, self.Y, self.y_removed, f)
         else:
             self.last_clf = self.clf
-            self.prediction_fit, self.prediction_proba_fit = self._last_fit(self.X_trans, self.Y, f=f)
+            self.prediction_fit, self.prediction_proba_fit = self._last_fit(self.X_trans, self.Y, self.y_removed, f=f)
             self.indiv_fit=None
  
         self.fitted = True
         f.close()    
+        self.Y = np.concatenate((self.Y, self.y_removed))
+        if len(self.X_removed) > 0:
+            self.X_trans = np.concatenate((self.X_trans, self.X_trans_removed))
         return self 
 
-    def predict(self, X_test, Y_test, scaler=None, imputer=None, train_folder="."):
+    def predict(self, X_test, Y_test, X_removed, y_removed, scaler=None, imputer=None, train_folder="."):
         assert self.fitted, "Please fit the model first"
         self.X_test = X_test
         self.Y_test = Y_test
@@ -244,17 +323,24 @@ class GenericModel(object):
         if scaler != None: self.scaler = scaler
         if imputer != None: self.imputer = imputer
         self.X_test_trans = self.scaler.transform(self.imputer.transform(self.X_test))
+        #for the removed molecules just imputing
+
+        if len(X_removed)>0:
+            X_trans_removed = np.zeros(shape=X_removed.shape)
         self.loaded_models = self.load_models()
 
         if self.stack:
             self.last_model = self.loaded_models[-1]
-            self._pipeline_predict(self.X_test_trans, self.Y_test)
+            self._pipeline_predict(self.X_test_trans, self.Y_test, y_removed)
             
         else:
             self.last_model = self.loaded_models[0]
-            self.prediction_test, self.predictions_proba_test = self._last_predict(self.X_test_trans)
+            self.prediction_test, self.predictions_proba_test = self._last_predict(self.X_test_trans, y_removed)
             self.indiv_pred = None
         self.results = [ pred == true for pred, true in zip(self.prediction_test, self.Y_test)] #last classifier
+        self.Y_test = np.concatenate((self.Y_test, y_removed))
+        if len(X_removed)>0:
+            self.X_test_trans = np.concatenate((self.X_test_trans, X_trans_removed))
 
         return self.results
 
