6e93284483544b97acd8b877e15c2e3388fa92c7
Anna Gruszka <frumpowy@gmail.com>
2021-04-07 14:53:48 +0200

    MOD-1 Doing my best here...

--- modtox/ML/external_descriptors.py
+++ modtox/ML/external_descriptors.py
@@ -3,11 +3,12 @@ import numpy as np
 import os
 from tqdm import tqdm
 
+
 class ExternalData():
 
-    def __init__(self, csv, mol_names, folder = '.', exclude=[]):
+    def __init__(self, csv, mol_names, folder='.', exclude=[]):
         self.csv = csv
-        self.mol_names = mol_names       
+        self.mol_names = mol_names
         self.exclude = exclude
         self.folder = folder
         if not os.path.exists(self.folder):
@@ -16,16 +17,16 @@ class ExternalData():
     def fit(self, molecules):
         return molecules
 
-    def fit_transform(self, molecules, labels): 
+    def fit_transform(self, molecules, labels):
         return self.transform(self.fit(molecules))
 
     def retrieve_mols_not_docked(self, molecules):
-        df = pd.read_csv(self.csv) 
+        df = pd.read_csv(self.csv)
         molecules_not_docked = []
         for i, title in enumerate(self.mol_names):
             if title not in df["Title"].values.astype(str):
-                #null_line = pd.DataFrame.from_records([values], columns=headers, index=[i])
-                #df = pd.concat([df.ix[:i-1], null_line, df.ix[i:]]).reset_index(drop=True)
+                # null_line = pd.DataFrame.from_records([values], columns=headers, index=[i])
+                # df = pd.concat([df.ix[:i-1], null_line, df.ix[i:]]).reset_index(drop=True)
                 molecules_not_docked.append(title)
         return molecules_not_docked
 
@@ -34,35 +35,34 @@ class ExternalData():
         print("\tIncorporating external data from {}".format(self.csv))
         df = pd.read_csv(self.csv)
         n_drop = 0
-        #If not present on your dataset discard
+        # If not present on your dataset discard
         self.mol_names = [m[0].GetProp("_Name") for m in molecules.values]
         for i, title in enumerate(df["Title"].values):
             if str(title) not in self.mol_names:
-                df.drop(df.index[i- n_drop], inplace=True)
+                df.drop(df.index[i - n_drop], inplace=True)
                 n_drop += 1
-        #check whether molecules in mol_names not in df
-        
- 
+        # check whether molecules in mol_names not in df
+
         headers = list(df)
         values = (None,) * len(headers)
-        #If not present on your glide 
+        # If not present on your glide
         print('Filling null lines...')
         for i, title in tqdm(enumerate(self.mol_names), total=len(self.mol_names)):
             if title not in df["Title"].values.astype(str):
                 null_line = pd.DataFrame.from_records([values], columns=headers, index=[i])
                 null_line["Title"] = str(title)
-                df = pd.concat([df.ix[:i-1], null_line, df.ix[i:]]).reset_index(drop=True)
-        #Drop features
-        df = df.replace("--",  np.nan)
+                df = pd.concat([df.iloc[:i - 1], null_line, df.iloc[i:]]).reset_index(drop=True)
+        # Drop features
+        df = df.replace("--", np.nan)
         df['Title'] = df.Title.astype(str)
         df = df.sort_values("Title")
-        features_to_drop = [feature for field in self.exclude for feature in headers if field in feature ]
+        features_to_drop = [feature for field in self.exclude for feature in headers if field in feature]
         df.drop(features_to_drop, axis=1, inplace=True)
         df.to_csv(os.path.join(self.folder, "model_features.txt"))
         return df
 
     def retrieve_molecule_names(self):
-        df = pd.read_csv(self.csv) 
-        thresh = int(df.shape[1]*0.8)
+        df = pd.read_csv(self.csv)
+        thresh = int(df.shape[1] * 0.8)
         df_dropna = df.dropna(thresh=thresh)
-        return df_dropna["Title"].values 
+        return df_dropna["Title"].values
