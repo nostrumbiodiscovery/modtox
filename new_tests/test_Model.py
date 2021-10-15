b174c4373c9eef78cdd3f309ef65102fff0b10cf
Jan Nadal <jannadalrz@gmail.com>
2021-10-15 12:06:34 +0200

    Tests rebuilt

--- new_tests/test_Model.py
+++ new_tests/test_Model.py
@@ -1,33 +1,49 @@
-from pandas.core.frame import DataFrame
-from sklearn.datasets import load_breast_cancer
-import random
-from pytest import approx
-
+from mock import patch
+import numpy as np
 from modtox.modtox.new_models_classes.ML.dataset import DataSet
-from modtox.modtox.new_models_classes.ML.model import Model
-from modtox.modtox.new_models_classes.ML.selector import ChiPercentileSelector
+from modtox.modtox.new_models_classes.ML.model import Model, ModelSummary
+from modtox.modtox.new_models_classes.ML.selector import ChiPercentileSelector, RFECrossVal
 from modtox.modtox.new_models_classes.ML.tuning import RandomHalvingSearch
+import os
+import pandas as pd
 
-data = load_breast_cancer(as_frame=True)
-df = data["data"]
-df["y"] = [bool(row) for row in data["target"]]
-l = [True] * 114 + [False] * 455
-random.shuffle(l)
-df["is_external"] = l
+DATA = os.path.join(os.path.dirname(__file__), "data", "ML_test")
+train_set_csv = os.path.join(DATA, "train_set.csv")
+test_set_csv = os.path.join(DATA, "test_set.csv")
+
+def to_Xy(csv):
+    df = pd.read_csv(csv, index_col=0)
+    X = df.drop("Activity", axis=1)
+    y = df["Activity"].to_numpy()
+    return X, y
+
+X_train, y_train = to_Xy(train_set_csv)
+X_test, y_test = to_Xy(test_set_csv)
+
+m = Model(X_train, y_train, X_test, y_test)
 
 # INTEGRATION TEST
-def test_Model():
-    m = Model(DataSet(df, y_col_name="y"))
-    m.select_features(ChiPercentileSelector)
-    m.train(RandomHalvingSearch)
-    m.external_set_validation()
-    assert m.summary["initial_features"] == 30
-    assert m.summary["selected_features"] == 15
-    assert m.summary["training_score"] == approx(0.98, abs=0.1)
-    assert m.summary["accuracy_ext_val"] == approx(0.95, abs=0.1)
-    assert m.summary["precision_ext_val"] == approx(0.95, abs=0.1)
+def test_build_model():
+    summ = m.build_model()
+    assert isinstance(summ.selector, RFECrossVal)
+    assert isinstance(summ.tuner, RandomHalvingSearch)
+    assert isinstance(summ.y_pred, np.ndarray)
+
+@patch("modtox.modtox.new_models_classes.ML.selector.RFECrossVal.get_scores", autospec=True)
+def test_plot_feature_selection(mock):
+    x = sorted(list(range(1, 4001, 10)), reverse=True)
+    y = np.random.uniform(low=0.75, high=0.95, size=(400,))
+    mock.return_value = x, y 
+    summ = ModelSummary(RFECrossVal(1000), "tuner", "estimator", "y_pred")
+    summ.plot_feature_selection()
+
+def test_get_estimators_accuracy():
+    summ = m.build_model(step=1000)
+    df = summ.get_estimators_accuracy(m.X_test, m.y_test)
+    # df.to_csv('classifier_scores.csv')
+    assert df.shape == (6, 6)
 
 
 
-
-    
+"""{'KNeighborsClassifier': 0.8057553956834532, 'SVC': 0.9280575539568345, 'LogisticRegression': 0.9136690647482014, 'DecisionTreeClassifier': 0.9712230215827338, 'BernoulliNB': 0.8633093525179856, 'VotingClassifier': 0.9424460431654677}
+"""
