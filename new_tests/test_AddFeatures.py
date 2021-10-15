6258e04c6db6eefe6f0175c40ff4d67bcc327c3e
Jan Nadal <jannadalrz@gmail.com>
2021-10-15 12:04:39 +0200

    Black formatting

--- new_tests/test_AddFeatures.py
+++ new_tests/test_AddFeatures.py
@@ -3,7 +3,11 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 import rdkit
 
-from modtox.modtox.new_models_classes.Features.add_feat import AddGlide, AddTopologicalFingerprints, AddMordred
+from modtox.modtox.new_models_classes.Features.add_feat import (
+    AddGlide,
+    AddTopologicalFingerprints,
+    AddMordred,
+)
 from modtox.modtox.new_models_classes.Features.feat_enum import Features
 
 from modtox.modtox.new_models_classes.mol import MoleculeFromChem
@@ -13,11 +17,15 @@ ACTIVES = os.path.join(DATA, "actives.sdf")
 INACTIVES = os.path.join(DATA, "inactives.sdf")
 GLIDE_CSV = os.path.join(DATA, "glide_features.csv")
 
+
 def test_format_glide():
-    g = AddGlide("a", "b", "c", glide_csv=GLIDE_CSV)  # Create object to access staticmethod
+    g = AddGlide(
+        "a", "b", "c", glide_csv=GLIDE_CSV
+    )  # Create object to access staticmethod
     df = g.format_glide(g.glide_csv)
-    model_df = pd.read_csv(os.path.join(DATA, 'formatted_glide.csv'), index_col=0)
+    model_df = pd.read_csv(os.path.join(DATA, "formatted_glide.csv"), index_col=0)
     assert_frame_equal(df, model_df)
+
 
 def test_add_glide():
     class TestMol:
@@ -31,18 +39,19 @@ def test_add_glide():
     g = AddGlide(mol1, mol2, mol3, glide_csv=GLIDE_CSV)
     ft_dict = g.calculate()
     assert list(ft_dict.keys()) == [mol1, mol2, mol3]
-    
-    assert (len(ft_dict[mol1]) == len(ft_dict[mol2]) == len(ft_dict[mol3]) == 168)
+
+    assert len(ft_dict[mol1]) == len(ft_dict[mol2]) == len(ft_dict[mol3]) == 168
 
     assert any(val != 0 for val in list(ft_dict[mol1].values()))
     assert any(val != 0 for val in list(ft_dict[mol2].values()))
     assert all(val == 0 for val in list(ft_dict[mol3].values()))
 
+
 def test_add_topo():
     class TestMol:
         def __init__(self, sm) -> None:
             self.molecule = rdkit.Chem.MolFromSmiles(sm)
-    
+
     mol1 = TestMol("CCN(CC)CC")
     mol2 = TestMol("C=CC(CCC)C(C(C)C)CCC")
 
@@ -50,14 +59,18 @@ def test_add_topo():
 
     ft_dict = g.calculate()
     assert list(ft_dict.keys()) == [mol1, mol2]
-    assert (len(ft_dict[mol1]) == len(ft_dict[mol2]) == 2048)
-    assert all(isinstance(mol.topo, rdkit.DataStructs.cDataStructs.ExplicitBitVect) for mol in [mol1, mol2])
+    assert len(ft_dict[mol1]) == len(ft_dict[mol2]) == 2048
+    assert all(
+        isinstance(mol.topo, rdkit.DataStructs.cDataStructs.ExplicitBitVect)
+        for mol in [mol1, mol2]
+    )
+
 
 def test_add_mordred():
     class TestMol:
         def __init__(self, sm) -> None:
             self.molecule = rdkit.Chem.MolFromSmiles(sm)
-    
+
     mol1 = TestMol("CCN(CC)CC")
     mol2 = TestMol("C=CC(CCC)C(C(C)C)CCC")
 
@@ -65,4 +78,4 @@ def test_add_mordred():
 
     ft_dict = g.calculate()
     assert list(ft_dict.keys()) == [mol1, mol2]
-    assert (len(ft_dict[mol1]) == len(ft_dict[mol2]) == 1613)
+    assert len(ft_dict[mol1]) == len(ft_dict[mol2]) == 1613
--- new_tests/test_HyperTuner.py
+++ new_tests/test_HyperTuner.py
@@ -1,9 +1,14 @@
 from pytest import approx
 from sklearn.datasets import load_breast_cancer
 from sklearn.ensemble import VotingClassifier
-from modtox.modtox.new_models_classes.ML.tuning import GridHalvingSearch, RandomHalvingSearch, RandomSearch
+from modtox.modtox.new_models_classes.ML.tuning import (
+    GridHalvingSearch,
+    RandomHalvingSearch,
+    RandomSearch,
+)
 
 X, y = load_breast_cancer(return_X_y=True)
+
 
 def test_tuners():
     tuner = RandomSearch(X, y)
@@ -12,6 +17,7 @@ def test_tuners():
     assert tuner.score == approx(0.93, abs=0.1)
     assert isinstance(tuner.best_params, dict)
 
+
 def test_random_halving():
     """Pretty quick"""
     tuner = RandomHalvingSearch(X, y)
@@ -19,6 +25,7 @@ def test_random_halving():
     assert isinstance(votclf, VotingClassifier)
     assert tuner.score == approx(0.96, abs=0.1)
     assert isinstance(tuner.best_params, dict)
+
 
 def test_grid_halving():
     """Really slow"""
