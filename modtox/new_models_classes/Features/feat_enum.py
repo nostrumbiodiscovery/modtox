8fb2cdb9fd0524af4fd05625594f6fb9ec182a78
Jan Nadal <jannadalrz@gmail.com>
2021-10-15 11:58:52 +0200

    Black formatting

--- modtox/new_models_classes/Features/feat_enum.py
+++ modtox/new_models_classes/Features/feat_enum.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
 
+
 class Features(Enum):
     glide = auto()  # glide features
     mordred = auto()  # mordred descriptors
-    topo = auto()  # rdkit topological fingerprints 
+    topo = auto()  # rdkit topological fingerprints
