bd426eef9e6e6bae3f6f39a3b4381f2d119859c2
Jan Nadal <jannadalrz@gmail.com>
2021-10-21 10:00:33 +0200

    MolFromInchi

--- modtox/Molecules/mol.py
+++ modtox/Molecules/mol.py
@@ -1,8 +1,12 @@
-from modtox.modtox.utils.enums import Features
+from modtox.modtox.Molecules.act import Activity
+from modtox.modtox.utils.enums import Features, StandardTypes
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from typing import Collection, Dict, List
 from dataclasses import dataclass
 from modtox.modtox.utils._custom_errors import FeatureError
+import modtox.modtox.constants.constants as k
+from modtox.modtox.utils._custom_errors import UnsupportedStandardType
 
 @dataclass
 class Cluster:
@@ -10,8 +14,10 @@ class Cluster:
     members: int
     is_centroid: bool
 
+
 class BaseMolecule:
     """Represents the base molecule. Should not instantiated."""
+
     molecule: Chem
     features: Dict[Features, Dict[str, float or int]]
     scaffold: str
@@ -38,7 +44,9 @@ class BaseMolecule:
         else:
             return False
 
-    def add_cluster_info(self, cluster_id: int, members: int, is_centroid: bool):
+    def add_cluster_info(
+        self, cluster_id: int, members: int, is_centroid: bool
+    ):
         self.cluster = Cluster(cluster_id, members, is_centroid)
 
     def add_feature(self, ft: Features, d: Dict[str, float or int]):
@@ -52,7 +60,7 @@ class BaseMolecule:
             Formatted as as: {'F1': 0, 'F2': 1.1, ...}"""
 
         self.features[ft] = d
-        
+
     def to_record(self, *features: Features):
         """Converts the molecule to a record format. Contains the activity,
         is_external and the specified features.If no *args are supplied, 
@@ -82,13 +90,13 @@ class BaseMolecule:
             "Activity": self.activity,
             "cluster_id": self.cluster.id,
             "cluster_members": self.cluster.members,
-            "is_centroid": self.cluster.is_centroid
-        }  
+            "is_centroid": self.cluster.is_centroid,
+        }
 
         for feature in features:
             if feature in self.features:
                 record.update(self.features[feature])
-            
+
             elif isinstance(feature, Features):
                 raise FeatureError(
                     f"Feature {feature.name!r} has not been calculated for the collection."
@@ -98,6 +106,7 @@ class BaseMolecule:
                     f"Feature {feature!r} is not an instance of Features(Enum) class."
                 )
         return record
+
 
 class MoleculeFromChem(BaseMolecule):
     """Molecule initialized from rdkit.Chem.Mol object with 
@@ -135,25 +144,66 @@ class MoleculeFromChem(BaseMolecule):
             False  # Default is False, extracting external set will set to True
         )
 
-class MoleculeFromSMILES(BaseMolecule):
-    """Molecule initialized from SMILE. Must supply activity and name."""
+
+class MoleculeFromInChI(BaseMolecule):
+    """Molecule initialized from InChI. Must supply InChI and name."""
 
     molecule: Chem
     scaffold: str
     name: str or None
 
     activity: bool
-    is_external: bool
-
+    activities: List[Activity]
     features: Dict[Features, Dict[str, float or int]]
-    similarities: List[float]
 
-    def __init__(self, smiles: str, activity: bool, name: str = None) -> None:
+    def __init__(self, inchi: str, name: str = None) -> None:
         super().__init__()
-        self.molecule = Chem.MolFromSmiles(smiles)
+        self.inchi = inchi
+        self.molecule = Chem.MolFromInchi(inchi, removeHs=True)
         self.name = name
+        self.activities = list()
+        self.activity_tags = dict()
 
-        self.activity = activity
-        self.is_external = (
-            False  # Default is False, extracting external set can set to True
-        )
+    def add_activity(self, activity):
+        self.activities.append(activity)
+
+    def assess_activity(self, standard_type: StandardTypes, threshold: float):
+        if standard_type == "pubchem":
+            self.ref_criterion = "pubchem"
+            return
+        
+        if standard_type not in StandardTypes:
+            raise UnsupportedStandardType(f"Standard {standard_type!r} not supported.")
+        
+        units = k.bdb_units[standard_type.name]
+
+        if not self.activity_tags:
+            self.ref_criterion = f"{standard_type.name}, {threshold} {units}"
+
+        is_active = list()
+        
+        for activity in self.activities:
+            if activity.standard.std_type == standard_type and activity.standard.std_val <= threshold:
+                is_active.append(True)
+            elif activity.standard.std_type == standard_type and activity.standard.std_val > threshold:
+                is_active.append(False)
+        
+        if not is_active:
+            self.activity_tags[f"{standard_type.name}, {threshold} {units}"] = "No data"
+        elif all(is_active):
+            self.activity_tags[f"{standard_type.name}, {threshold} {units}"] = "Active"
+        elif any(is_active) == False:
+            self.activity_tags[f"{standard_type.name}, {threshold} {units}"] = "Inactive"
+        else:
+            self.activity_tags[f"{standard_type.name}, {threshold} {units}"] = "Contradictory data"
+
+    def _add_properties_to_molecule(self):
+        Chem.AssignStereochemistry(self.molecule)
+        AllChem.EmbedMolecule(self.molecule)
+        self.molecule.SetProp("_MolFileChiralFlag","1")
+        self.molecule.SetProp("_Name", self.name)
+        self.molecule.SetProp("InChI", self.inchi)
+        try:
+            self.molecule.SetProp("Activity", self.activity_tags.get(self.ref_criterion))
+        except AttributeError:
+            pass
